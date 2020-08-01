eeeca58a2c4d107f7b6f8d1169b73378f9f7ff93
Jannis Mattheis <contact@jmattheis.de>
2020-08-01 15:32:25 +0200

    Make keepalive period configurable

--- runner/runner.go
+++ runner/runner.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gotify/server/v2/config"
@@ -26,6 +28,7 @@ func Run(engine *gin.Engine, conf *config.Configuration) {
 			Addr:    addr,
 			Handler: engine,
 		}
+		setKeepAlivePeriod(s, conf.Server.KeepAlivePeriodSeconds)
 
 		if *conf.Server.SSL.LetsEncrypt.Enabled {
 			certManager := autocert.Manager{
@@ -43,7 +46,30 @@ func Run(engine *gin.Engine, conf *config.Configuration) {
 	}
 	addr := fmt.Sprintf("%s:%d", conf.Server.ListenAddr, conf.Server.Port)
 	fmt.Println("Started Listening for plain HTTP connection on " + addr)
-	log.Fatal(http.ListenAndServe(addr, httpHandler))
+	server := &http.Server{Addr: addr, Handler: httpHandler}
+	setKeepAlivePeriod(server, conf.Server.KeepAlivePeriodSeconds)
+
+	conn, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Println("Could not start http server", err)
+		os.Exit(1)
+	}
+
+	log.Fatal(server.Serve(conn))
+}
+
+func setKeepAlivePeriod(srv *http.Server, periodSeconds int) {
+	if periodSeconds == 0 {
+		return
+	}
+
+	srv.ConnState = func(conn net.Conn, state http.ConnState) {
+		if state == http.StateNew {
+			if err := conn.(*net.TCPConn).SetKeepAlivePeriod(time.Duration(periodSeconds) * time.Second); err != nil {
+				fmt.Println("Could not set keep alive period", err)
+			}
+		}
+	}
 }
 
 func redirectToHTTPS(port string) http.HandlerFunc {
